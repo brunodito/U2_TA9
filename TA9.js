0a21b1079d2dfa90d6aa46272843570a3d07ec28
Bruno <brunitodito@gmail.com>
2024-08-20 10:09:00 -0300

    Cambios

---
+++ TA9.js
@@ -0,0 +1,6 @@
+function getOdds (nums) {
+    let numImpar = nums.filter(num => num % 2 != 0)
+    console.log(numImpar)
+}
+const arrayAleatorio = [34, 7, 23, 89, 56, 12, 45, 78, 90, 2];
+getOdds(arrayAleatorio)
